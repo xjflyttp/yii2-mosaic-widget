3e720edf856e9181f811de22836dc17f4034cc50
xjflyttp <xjflyttp@gmail.com>
2015-03-06 16:14:05 +0800

    update

--- MosaicAsset.php
+++ MosaicAsset.php
@@ -6,25 +6,10 @@ use yii\web\AssetBundle;
 
 class MosaicAsset extends AssetBundle {
 
-    public $sourcePath;
+    public $sourcePath = '@vendor/xj/yii2-mosaic-widget/assets';
     public $basePath = '@webroot/assets';
-    public $publishOptions = ['forceCopy' => YII_DEBUG];
     public $css = ['css/mosaic.css'];
-    public $js = [];
+    public $js = ['js/mosaic.1.0.1.js'];
     public $depends = ['yii\web\JqueryAsset'];
-
-    private function getJs() {
-        return [
-            YII_DEBUG ? 'js/mosaic.1.0.1.js' : 'js/mosaic.1.0.1.min.js',
-        ];
-    }
-
-    public function init() {
-        $this->sourcePath = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'assets';
-        if (empty($this->js)) {
-            $this->js = $this->getJs();
-        }
-        return parent::init();
-    }
 
 }
