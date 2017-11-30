4262cc2d266f0e2d9914aff37c41be82680edde5
xjflyttp <xjflyttp@gmail.com>
2017-11-30 10:57:38 +0800

    fix basePath

--- MosaicAsset.php
+++ MosaicAsset.php
@@ -7,7 +7,6 @@ use yii\web\AssetBundle;
 class MosaicAsset extends AssetBundle {
 
     public $sourcePath = '@vendor/xj/yii2-mosaic-widget/assets';
-    public $basePath = '@webroot/assets';
     public $css = ['css/mosaic.css'];
     public $js = ['js/mosaic.1.0.1.js'];
     public $depends = ['yii\web\JqueryAsset'];
